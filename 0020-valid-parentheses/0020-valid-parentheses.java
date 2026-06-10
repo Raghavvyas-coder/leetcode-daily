2f007cd24d18a1c823fdb8f00acbe5c3dc5e8265
Raghav Vyas <raghavyas2005@gmail.com>
2026-06-11 00:58:02 +0530

    Time: 3 ms (87.85%), Space: 43.3 MB (40.08%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,39 +1,27 @@
-import java.util.*;
+import java.util.Stack;
 
 class Solution {
     public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
 
-        Stack<Character> st = new Stack<>();
-
-        for(int i = 0; i < s.length(); i++) {
-
-            char ch = s.charAt(i);
-
-            if(ch == '(' || ch == '{' || ch == '[') {
-                st.push(ch);
-            }
-            else {
-
-                if(st.isEmpty()) {
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty()) {
                     return false;
                 }
 
-                if(ch == ')' && st.peek() != '(') {
+                char top = stack.pop();
+
+                if ((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '[')) {
                     return false;
                 }
-
-                if(ch == '}' && st.peek() != '{') {
-                    return false;
-                }
-
-                if(ch == ']' && st.peek() != '[') {
-                    return false;
-                }
-
-                st.pop();
             }
         }
 
-        return st.isEmpty();
+        return stack.isEmpty();
     }
 }
